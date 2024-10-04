a33edb7192cb01fd261fbf3c2cbfbac8569c70fd
Vamsi Krishna Kolli <vamsi@onemyle.com>
2024-10-04 14:18:49 +0530

    Initial commit

---
+++ app/connect/page.tsx
@@ -0,0 +1,16 @@
+import About from "@/components/About";
+import Banner from "@/components/Banner";
+import Experience from "@/components/Experience";
+import Footer from "@/components/Footer";
+import Projects from "@/components/Projects";
+
+export default function Home() {
+  return (
+    <main className="h-full w-full  bg-[url('/LooperGroup2.png')] bg-no-repeat">
+      <div className="flex flex-col gap-20">
+        <iframe src="https://linktr.ee/vamc.in" title="Linktree"></iframe>
+        <Footer />
+      </div>
+    </main>
+  );
+}
